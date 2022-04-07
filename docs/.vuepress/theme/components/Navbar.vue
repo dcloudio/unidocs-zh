ef48bbeadf07c0cf71ce510004de2072c6133cf2
DCloud_LXH <283700113@qq.com>
2022-04-07 17:00:35 +0800

    chore: navbar remove switchVersion

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -34,7 +34,7 @@
           'max-width': linksWrapMaxWidth + 'px'
         } : {}"
       >
-        <a class="switch-version" href="javascript:void(0)" @click="switchVersion">回到旧版</a>
+        <!-- <a class="switch-version" href="javascript:void(0)" @click="switchVersion">回到旧版</a> -->
         <DcloudSearchPage v-if="isAlgoliaSearch" ref="dcloudSearchPage" :options="algolia"/>
         <AlgoliaSearchBox v-if="isAlgoliaSearch" />
         <SearchBox v-else-if="$site.themeConfig.search !== false && $page.frontmatter.search !== false" />

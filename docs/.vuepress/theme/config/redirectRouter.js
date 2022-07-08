e70a21ff39849b1fdc486bba5b3cd32c5e1c0b80
linju <linju@dcloud.io>
2022-07-08 04:15:51 +0000

    新增pages-init文档重定向

--- docs/.vuepress/theme/config/redirectRouter.js
+++ docs/.vuepress/theme/config/redirectRouter.js
@@ -76,6 +76,7 @@ const routerMap = {
   '/uniCloud/uni-clientDB': '/uniCloud/clientdb.html',
   '/uniCloud/uni-data-picker': '/component/uniui/uni-data-picker.html',
   '/uniCloud/uni-push/introduction.html': '/unipush-v2.html',
+  '/plugin/publish.html#pages-init':'https://uniapp.dcloud.io/plugin/uni_modules.html#pages-init',
 
   '/plugin/hybrid': '/hybrid.html',
   '/adapt': '/tutorial/adapt.html',

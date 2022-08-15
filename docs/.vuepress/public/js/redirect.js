6e3aed74e72f2741e7726aa841e7baaedd713c5f
DCloud_LXH <283700113@qq.com>
2022-08-15 19:41:51 +0800

    docs: static js

--- docs/.vuepress/public/js/redirect.js
+++ docs/.vuepress/public/js/redirect.js
@@ -5,6 +5,7 @@ var website_ZH_host = 'uniapp.dcloud.net.cn'
 var website_EN_host = 'en.uniapp.dcloud.io'
 var website_ZH = 'https://' + website_ZH_host;
 var website_EN = 'https://' + website_EN_host;
+var _hmt = _hmt || [];
 
 // window.__UNI_DOCS_ZH_KEY__ = 'unidocs-zh-language'
 

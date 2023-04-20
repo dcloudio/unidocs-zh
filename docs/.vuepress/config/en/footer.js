2189c47509d6334696b500181857713975392b55
DCloud_LXH <283700113@qq.com>
2023-04-20 16:36:41 +0800

    fix: footer config

--- docs/.vuepress/config/en/footer.js
+++ docs/.vuepress/config/en/footer.js
@@ -28,6 +28,10 @@ export default {
           url: 'https://uniapp.dcloud.net.cn/uniCloud/uni-id',
         },
         {
+          subTitle: 'uni-search',
+          url: 'https://ext.dcloud.net.cn/plugin?id=3851',
+        },
+        {
           subTitle: 'uniPay',
           url: 'https://uniapp.dcloud.net.cn/uniCloud/unipay',
         },
@@ -55,6 +59,18 @@ export default {
           subTitle: 'uni-upgrade-center',
           url: 'https://uniapp.dcloud.io/uniCloud/upgrade-center',
         },
+        {
+          subTitle: 'uni-im',
+          url: 'https://uniapp.dcloud.net.cn/uniCloud/uni-im',
+        },
+        {
+          subTitle: 'uni-ai',
+          url: 'https://uniapp.dcloud.net.cn/uniCloud/uni-ai',
+        },
+        {
+          subTitle: 'uni-cms',
+          url: 'https://uniapp.dcloud.net.cn/uniCloud/uni-cms',
+        }
       ],
     },
   ],
@@ -75,22 +91,14 @@ export default {
           url: 'https://www.dcloud.io/dportal.html',
         },
         {
-          subTitle: '',
-          url: '',
-        },
-        {
-          subTitle: '',
-          url: '',
-        },
-        {
-          subTitle: '',
-          url: '',
-        },
+          subTitle: 'Safety topic',
+          url: 'https://uniapp.dcloud.net.cn/tutorial/safe.html',
+        }
       ],
     },
     /* {
-       'title': '',
-       'content': []
+      'title': '',
+      'content': []
      }, */
     {
       title: 'Developer Services',
@@ -138,47 +146,47 @@ export default {
           url: 'https://open.dcloud.net.cn/pages/login/login',
         },
       ],
-    },
-    {
-      title: 'About',
-      content: [
-        {
-          subTitle: 'DCloud',
-          url: 'https://dcloud.io/',
-        },
-		{
-		  subTitle: 'Jobs',
-		  url: 'https://www.dcloud.io/hr/',
-		},
-        {
-          subTitle: 'Terms of service',
-          url: 'https://account.dcloud.io/privacy-policy/DCloud-Terms-of-Service.html',
-        },
-		{
-		  subTitle: 'Privacy policy',
-		  url: 'https://account.dcloud.io/privacy-policy/DCloud-Privacy-Policy.html',
-		},
-		{
-		  subTitle: 'acceptable use policy',
-		  url: 'https://account.dcloud.io/privacy-policy/DCloud-Acceptable-Use-Policy.html',
-		}
-        
-      ],
-    },
-    {
-      title: 'Contact us',
-      content: [
-        {
-          subTitle: 'bussiness：bd@dcloud.io',
-          url: 'mailto:bd@dcloud.io',
-        },
-        {
-          subTitle: 'Advertising：uniad@dcloud.io',
-          url: 'mailto:uniad@dcloud.io',
-        },
-      ],
-    },
+    }
   ],
+  aboutus: {
+    title: 'About',
+    content: [
+      {
+        subTitle: 'DCloud',
+        url: 'https://dcloud.io/',
+      },
+      {
+        subTitle: 'Jobs',
+        url: 'https://www.dcloud.io/hr/',
+      },
+      {
+        subTitle: 'Terms of service',
+        url: 'https://account.dcloud.io/privacy-policy/DCloud-Terms-of-Service.html',
+      },
+      {
+        subTitle: 'Privacy policy',
+        url: 'https://account.dcloud.io/privacy-policy/DCloud-Privacy-Policy.html',
+      },
+      {
+        subTitle: 'acceptable use policy',
+        url: 'https://account.dcloud.io/privacy-policy/DCloud-Acceptable-Use-Policy.html',
+      }
+
+    ],
+  },
+  concatus: {
+    title: 'Contact us',
+    content: [
+      {
+        subTitle: 'bussiness：bd@dcloud.io',
+        url: 'mailto:bd@dcloud.io',
+      },
+      {
+        subTitle: 'Advertising：uniad@dcloud.io',
+        url: 'mailto:uniad@dcloud.io',
+      },
+    ],
+  },
   domainImg: 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f184e7c3-1912-41b2-b81f-435d1b37c7b4/769929a3-65eb-4d11-815d-84f88197a152.png',
   beian: 'http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802035340'
 }

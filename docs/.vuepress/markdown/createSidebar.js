629d7e6e84e21c9e5e2023ee99aa54b7aaa88b94
DCloud_LXH <283700113@qq.com>
2023-12-11 17:28:40 +0800

    feat: create sitemap

--- docs/.vuepress/markdown/createSidebar.js
+++ docs/.vuepress/markdown/createSidebar.js
@@ -46,7 +46,7 @@ function parseBar(tab, file, options) {
             }
           }
 
-          link = path.join('/', link.replace(/\.md\b/, '.html')
+          link = path.join('/', link.replace(/\.md\b/, '')
             .replace(/\bREADME\b/, '')
             .replace(/\/index/, '/')
             .replace(/\?id=/, '#'))

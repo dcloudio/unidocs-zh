b041c99cfca62a300c3f9169384f333576059403
DCloud_LXH <283700113@qq.com>
2025-05-10 18:52:23 +0800

    fix: img-add-attrs

--- docs/.vuepress/markdown/img-add-attrs.js
+++ docs/.vuepress/markdown/img-add-attrs.js
@@ -1,36 +1,61 @@
 module.exports = function (md, opts) {
-	md.core.ruler.after('inline', 'image-attrs', function (state) {
-		state.tokens.forEach(function (blockToken) {
-			if (blockToken.type === 'html_block') {
-				replaceHTML(blockToken, addLoadingAttr, state.env);
-			}
-			if (blockToken.type === 'inline' && blockToken.children) {
-				blockToken.children.forEach(function (token) {
-					const type = token.type;
-					if (type === 'html_inline') {
-						replaceHTML(token, addLoadingAttr, state.env);
-					}
-					if (type === 'image') {
-						const src = token.attrGet('src');
-						if (!token.attrGet('loading') && matchSrc(src)) {
-							token.attrPush(['loading', 'lazy']);
-						}
-					}
-				});
-			}
-		});
-		return false;
-	});
-};
+  const oldRendererImageRule = md.renderer.rules.image
+  md.renderer.rules.image = function (tokens, idx, ...args) {
+    var token = tokens[idx]
+    var url = token.attrGet('src')
+    if (!token.attrGet('loading') && matchSrc(url)) {
+      token.attrPush(['loading', 'lazy'])
+    }
+    return oldRendererImageRule(tokens, idx, ...args)
+  }
+  md.core.ruler.after('inline', 'image-attrs', function (state) {
+    let joinTokenContent = false
+    let handleToken = null
+    for (let i = 0; i < state.tokens.length; i++) {
+      const blockToken = state.tokens[i]
+      if (blockToken.type === 'html_block') {
+        if (joinTokenContent && handleToken) {
+          handleToken.content += blockToken.content
+          state.tokens.splice(i, 1)
+          i--
+        } else {
+          if (i === state.tokens.length - 1) {
+            // 如果是最后一个token，直接替换
+            replaceHTML(blockToken, addLoadingAttr, state.env)
+          } else {
+            joinTokenContent = true
+            handleToken = blockToken
+          }
+        }
+      } else {
+        joinTokenContent = false
+        if (handleToken) {
+          replaceHTML(handleToken, addLoadingAttr, state.env)
+          handleToken = null
+        }
+
+        if (blockToken.type === 'inline' && blockToken.children) {
+          blockToken.children.forEach(function (token) {
+            const type = token.type
+            if (type === 'html_inline') {
+              replaceHTML(token, addLoadingAttr, state.env)
+            }
+          })
+        }
+      }
+    }
+    return false
+  })
+}
 
 /**
  *
  * @param {Record<string,any>} attribs
  */
 function addLoadingAttr(attribs) {
-	if (matchSrc(attribs.src) && !attribs.loading) {
-		attribs.loading = 'lazy';
-	}
+  if (matchSrc(attribs.src) && !attribs.loading) {
+    attribs.loading = 'lazy'
+  }
 }
 
 /**
@@ -39,34 +64,35 @@ function addLoadingAttr(attribs) {
  * @returns
  */
 function matchSrc(src) {
-	return typeof src === 'string'
-		? (src.indexOf('qiniu-web-assets.dcloud.net.cn') > -1 || src.indexOf('web-ext-storage.dcloud.net.cn') > -1)
-		: false;
+  return typeof src === 'string' && ['qiniu-web-assets.dcloud.net.cn','web-ext-storage.dcloud.net.cn'].some((item) => src.includes(item))
 }
 
 function replaceNodes(nodes, replace, env, token) {
-	if (!nodes) return;
-	for (let i = 0; i < nodes.length; i++) {
-		const node = nodes[i];
-		if (node.attribs) {
-			if (node.name === 'img' && node.attribs.src) {
-				replace(node.attribs);
-			}
-		}
-		replaceNodes(node.children, replace, env, token);
-	}
+  if (!nodes) return
+  for (let i = 0; i < nodes.length; i++) {
+    const node = nodes[i]
+    if (node.attribs) {
+      if (node.name === 'img' && node.attribs.src) {
+        replace(node.attribs)
+      }
+    }
+    replaceNodes(node.children, replace, env, token)
+  }
 }
 
 function replaceHTML(token, replace, env) {
-	const htmlparser = require('htmlparser2');
-	const serializer = require('dom-serializer');
-	const dom = new htmlparser.parseDocument(token.content, {
-		lowerCaseTags: false,
-		recognizeCDATA: true,
-		recognizeSelfClosing: true,
-	});
-	// 会将 <a> 标签解析成 <a></a>，所以要判断一下
-	if (!dom.firstChild || dom.firstChild.name === 'a') return
-	replaceNodes(dom.children, replace, env, token);
-	token.content = serializer.render(dom, { encodeEntities: 'utf8' });
+  const htmlparser = require('htmlparser2')
+  const serializer = require('dom-serializer')
+  const dom = new htmlparser.parseDocument(token.content, {
+    lowerCaseTags: false,
+    xmlMode: true,
+    decodeEntities: false,
+  })
+  /**
+   * 会将 <a/> 标签解析成 <a></a>
+   * 会将 <style></style> 标签解析
+   */
+  if (!dom.firstChild || ['a', 'style'].includes(dom.firstChild.name)) return
+  replaceNodes(dom.children, replace, env, token)
+  token.content = serializer.render(dom, { encodeEntities: false, xmlMode: 'foreign' })
 }

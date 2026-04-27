0354f0ef474725d7c295f51912bfb641d22352b1
DCloud_LXH <283700113@qq.com>
2026-04-27 18:11:55 +0800

    feat: uni-agent landing page

--- docs/.vuepress/config/toc.js
+++ docs/.vuepress/config/toc.js
@@ -1,5 +1,13 @@
 export default {
   onThisPage: '本页导读',
   collapseText: '展示全部',
-  expandText: '收起目录'
+  expandText: '收起目录',
+  ads: [
+    {
+      img: 'https://web-ext-storage.dcloud.net.cn/doc/ai/uni-agent-landing-page-compress.png',
+      href: 'https://uniapp.dcloud.net.cn/ai/uni-agent.html',
+      alt: 'uni-agent',
+      // target: '_blank', // 可选，默认 _blank
+    }
+  ],
 }

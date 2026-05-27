515c160b0a26d0d82e554601c2456c67adb717d0
lvqing <lvqing@dcloud.io>
2026-05-27 17:51:11 +0800

    update:docs-sync.config.ts

--- docs-sync.config.ts
+++ docs-sync.config.ts
@@ -33,6 +33,7 @@ export default defineConfig({
       'ai/vibe-partner.md',
       'ai/use-ui-mcp.md',
       'ai/personal-subscription.md',
+	  'ai/enterprise-subscription.md',
     ]),
     // worktile
     ...createSyncMapping([
